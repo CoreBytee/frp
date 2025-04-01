bf534f7868caa19e35da203cf38931fc7e6b95c3
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-04-01 21:29:56 +0200

    change binaries

---
+++ src/cli/frpc.ts
@@ -0,0 +1,9 @@
+import { spawnSync } from "node:child_process";
+import { platform } from "node:os";
+import { join } from "node:path";
+
+const isWindows = platform() === "win32";
+const executable = `${join(import.meta.dir, "../../.binaries/frpc")}${isWindows ? ".exe" : ""}`;
+const args = process.argv.slice(2); // Pass all input arguments to the executable
+
+spawnSync(executable, args, { stdio: "inherit" });
